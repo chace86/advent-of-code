e569840d23ea26f48151728d52298fa4621b0ce8
Chace Anderson <chace.5008@gmail.com>
2024-12-29 00:45:00 -0500

    add day 3

---
+++ 2024/day3/day3.py
@@ -0,0 +1,17 @@
+import re
+
+memory = open("day3-data.txt").read()
+tuples = re.findall("mul\((\d{1,3}),(\d{1,3})\)|(do)\(\)|(don't)\(\)", memory)
+total = 0
+
+active = True
+for tuple in tuples:
+    if "do" in tuple:
+        active = True
+    elif "don't" in tuple or not active:
+        active = False
+    else:
+        total += (int(tuple[0]) * int(tuple[1]))
+
+
+print(total)
